a06420f97930b45c95765d8e8fb3a3b62f587822
Fatima <en.fh90@gmail.com>
2018-02-27 00:34:18 +0200

    init

---
+++ accum.js
@@ -0,0 +1,9 @@
+// This time no story, no theory. The examples below show you what do you need, write a function to accomplich that
+// Examples:
+// accum("abcd");    // "A-Bb-Ccc-Dddd"
+// accum("RqaEzty"); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt");    // "C-Ww-Aaa-Tttt"
+
+    function accum(input){
+        //your code is here
+    }
